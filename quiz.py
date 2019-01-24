254b4bd804e95b44f7c7cc5dd110e03b95fd9d3b
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-01-24 13:48:08 +0000

    relative and absolute paths

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
-f = open('data.txt', 'r')       #open the file
+#f = open('files/relative_data.txt', 'r')       #open the file.....relative path
+f = open('/home/ubuntu/workspace/files/relative_data.txt', 'r')     #absolute path
 lines = f.read()           #read gives 4 seperate lines. appears as text not as a list of strings(like readlines does)                         #readlines gives ['This is the first line\n', 'Andthis is the second line\n', 'Here is the                             third line\n', 'And here, the fourth'](4 seperate strings)
 f.close()
 print(lines)
